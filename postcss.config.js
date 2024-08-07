8223f4c0ff32609a86936826707e2d6088ce2eee
Derrick Yew <yewcm123@gmail.com>
2024-08-07 23:52:58 +0800

    Added prettier and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-preset-env": {
+      stage: 0,
+      features: {
+        "nesting-rules": true,
+      },
+    },
+    "postcss-css-variables": {},
   },
-}
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      outfit: [
-        'Outfit',
-        'sans-serif'
-      ]
+      outfit: ["Outfit", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
