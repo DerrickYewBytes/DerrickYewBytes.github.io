c055d6b2d7a2428ff8caf35f10d0ac7f4d030143
Derrick Yew <yewcm123@gmail.com>
2024-08-26 22:26:17 +0800

    added font config and cursor pointers to links

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ConfigProvider } from "antd";
 import "./styles/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "Outfit",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 );
