fece6bf016061cf7110ae1366b01e0215523648e
Derrick Yew <yewcm123@gmail.com>
2024-08-26 23:29:18 +0800

    updated github link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,2 +1,2 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/derrick-yewcm/";
-export const GITHUB_URL = "https://github.com/yewcm123";
+export const GITHUB_URL = "https://github.com/DerrickYewBytes";
